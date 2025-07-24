4922bc05d1b33df915c69e2c81f2177e0f076166
Nathan Foo <ntfoo27@outlook.com>
2025-07-24 09:45:41 -0500

    dashboard and create page

---
+++ src/app/(pages)/(routes)/create/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { UploadDropzone } from "@/utils/uploadthing";
+import { toast } from "sonner";
+
+const CreatePage = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-[200vh]">
+            Create
+            <UploadDropzone
+                endpoint="pdfUploader"
+                onClientUploadComplete={(res) => {
+                    // Do something with the response
+                    console.log("Files: ", res);
+                    toast("Upload complete!");
+                }}
+                onUploadError={(error: Error) => {
+                    // Do something with the error.
+                    toast("Upload error.");
+                }}
+            />
+        </div>
+    )
+}
+
+export default CreatePage
---
+++ src/app/(pages)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const DashboardPage = () => {
+    return (
+        <div className="flex flex-col items-center justify-center h-[200vh]">
+            Dashboard
+        </div>
+    )
+}
+
+export default DashboardPage
