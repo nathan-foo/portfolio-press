1e685cb52e61a5ff8ce21d74a6a93139353cb4ad
Nathan Foo <ntfoo27@outlook.com>
2025-07-25 15:26:14 -0500

    project list

--- src/app/(pages)/(routes)/projects/_components/sidebar.tsx
+++ src/app/(pages)/(routes)/projects/_components/sidebar.tsx
@@ -1,3 +1,8 @@
+"use client"
+
+import { useTRPC } from '@/trpc/client';
+import { useQuery, useSuspenseQuery } from '@tanstack/react-query';
+import Link from 'next/link';
 import React from 'react'
 
 interface Props {
@@ -5,9 +10,27 @@ interface Props {
 }
 
 export const Sidebar = ({ projectId }: Props) => {
+    const trpc = useTRPC();
+    const { data: projects } = useQuery(trpc.projects.getMany.queryOptions());
+
+    console.log(projects)
+
     return (
-        <div className='w-full'>
-            projects
+        <div className='w-full p-4 flex-1 overflow-auto'>
+            <h1 className='text-xl font-semibold pb-4 p-2'>
+                Your Projects
+            </h1>
+            <div className='flex flex-col gap-y-2'>
+                {projects?.map((project, index) => (
+                    <Link
+                        key={index}
+                        href={`/projects/${project.id}`}
+                        className={`p-2 rounded-lg ${project.id === projectId && 'bg-muted'}`}
+                    >
+                        {project.name}
+                    </Link>
+                ))}
+            </div>
         </div>
     )
 }
